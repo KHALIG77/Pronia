0e0221ea059cf0f48c114ef8c3bd9370fb31d4cc
Khalig <khaligfm@code.edu.az>
2023-06-02 04:17:59 +0400

    Layout Wroot

---
+++ Pronia/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Pronia.Models;
+
+namespace Pronia.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();	
+		}
+	}
+}
