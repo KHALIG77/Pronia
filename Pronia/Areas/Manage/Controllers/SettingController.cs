d3767b10637e7708c0e828015642e2f83cafcbd6
Khalig <khaligfm@code.edu.az>
2023-06-11 00:09:12 +0400

    Seting Admin

--- Pronia/Areas/Manage/Controllers/SettingController.cs
+++ Pronia/Areas/Manage/Controllers/SettingController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pronia.Areas.Manage.ViewModels;
+using Pronia.DAL;
+using Pronia.Helper.FileManager;
+using Pronia.Models;
 
 namespace Pronia.Areas.Manage.Controllers
 {
@@ -6,9 +10,92 @@ namespace Pronia.Areas.Manage.Controllers
 
     public class SettingController : Controller
     {
+        private readonly ProniaContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public SettingController(ProniaContext context,IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
         public IActionResult Index()
         {
-            return View();
+
+            SettingsViewModel setting = new SettingsViewModel()
+            {
+                Address = _context.Settings.FirstOrDefault(x => x.Key == "Address").Value,
+                ContactPhone = _context.Settings.FirstOrDefault(x => x.Key == "ContactPhone").Value,
+                SupportPhone = _context.Settings.FirstOrDefault(x => x.Key == "SupportPhone").Value,
+                HeaderLogo = _context.Settings.FirstOrDefault(x => x.Key == "HeaderLogo").Value,
+                FooterLogo = _context.Settings.FirstOrDefault(x => x.Key == "FooterLogo").Value,
+
+            };
+            return View(setting);
+           
+          
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(SettingsViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            string oldHeaderName = null;
+            if (model.FileHeader!=null)
+            {
+                if(model.HeaderLogo!=null)
+                {
+                    oldHeaderName = model.HeaderLogo;
+                }
+                model.HeaderLogo = FileManager.Save(_env.WebRootPath, "uploads/settings", model.FileHeader);
+            }
+            string oldFooterName = null;
+            if (model.FileFooter != null)
+            {
+                if (model.FileFooter != null)
+                {
+                    oldFooterName = model.FooterLogo;
+                }
+                model.FooterLogo = FileManager.Save(_env.WebRootPath, "uploads/settings", model.FileFooter);
+            }
+            if (model.FileHeader == null&& model.HeaderLogo == null)
+            {
+                var check = _context.Settings.FirstOrDefault(x => x.Key =="HeaderLogo")?.Value;
+                if (check != null)
+                {
+                    oldHeaderName = check;
+                }
+            }
+            if (model.FileHeader == null && model.FooterLogo == null)
+            {
+                var check = _context.Settings.FirstOrDefault(x => x.Key == "FooterLogo")?.Value;
+                if (check != null)
+                {
+                    oldFooterName = check;
+                }
+            }
+            _context.Settings.FirstOrDefault(x => x.Key == "Address").Value = model.Address;
+            _context.Settings.FirstOrDefault(x => x.Key == "ContactPhone").Value = model.ContactPhone;
+            _context.Settings.FirstOrDefault(x => x.Key == "SupportPhone").Value = model.SupportPhone;
+            _context.Settings.FirstOrDefault(x => x.Key == "HeaderLogo").Value = model.HeaderLogo;
+            _context.Settings.FirstOrDefault(x => x.Key == "FooterLogo").Value = model.FooterLogo;
+
+            _context.SaveChanges();
+
+
+            if (oldFooterName != null)
+            {
+                FileManager.Delete(_env.WebRootPath, "uploads/settings", oldFooterName);
+            }
+            if (oldHeaderName!=null){
+                FileManager.Delete(_env.WebRootPath, "uploads/settings", oldHeaderName);
+
+            }
+
+
+            return RedirectToAction("index");
         }
     }
 }
---
+++ Pronia/Areas/Manage/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Pronia.Attiributes.ValidationAttiributes;
+
+namespace Pronia.Areas.Manage.ViewModels
+{
+    public class SettingsViewModel
+    {
+        [MaxLength(60)]
+        public string Address {get; set;}
+        [MaxLength(60)]
+        public string ContactPhone {get; set;}
+        [MaxLength(60)]
+        public string SupportPhone {get; set;}
+        [NotMapped]
+        [AllowFileType("image/jpg","image/jpeg","image/png")]
+        [FileSize(1000000)]
+        public IFormFile FileHeader {get; set;}
+        [NotMapped]
+        [AllowFileType("image/jpg", "image/jpeg", "image/png")]
+        [FileSize(1000000)]
+        public IFormFile FileFooter {get; set;}
+        [MaxLength(100)]
+        public string HeaderLogo {get; set;}
+        [MaxLength(100)]
+        public string FooterLogo {get; set;}
+    }
+}
